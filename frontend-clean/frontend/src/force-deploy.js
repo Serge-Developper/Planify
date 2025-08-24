5d2deeeede564c8dd203815735c034ff5aff8f5c
Worloftt <worloftsergetovarm0@gmail.com>
2025-08-24 04:02:04 +0200

    Fix: ajout de la fonction getDynamicItemVariant manquante dans le store coins

--- frontend-clean/frontend/src/force-deploy.js
+++
@@ -1,6 +0,0 @@
-// Fichier temporaire pour forcer le déploiement Netlify
-// Les fonctions Netlify ont été mises à jour et doivent être redéployées
-// Date: 2025-01-24
-// Commit: 6cfebf1 - Refactorisation: fonction coins.js unifiée
-
-console.log('Force deploy triggered');
